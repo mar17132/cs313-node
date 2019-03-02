d00106c0cb4a5609f801324f12b13b019fff8489
mar17132 <mar17132@byui.edu>
2019-03-02 09:35:53 -0700

    ggg

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   .get('/math', (req, res) => res.sendfile('html/static_form.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
