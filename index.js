7c6882da58c72a9158934760f44d0502338f072a
mar17132 <mar17132@byui.edu>
2019-03-02 09:26:53 -0700

    fff

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use('/math',express.static(path.join(__dirname, 'html')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
